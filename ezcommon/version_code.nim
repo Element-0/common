25b3d751548b7086cd3d775377699250903167c7
CodeHz <codehz@outlook.com>
2021-01-25 00:04:28 +0800

    fix parsing

--- ezcommon/version_code.nim
+++ ezcommon/version_code.nim
@@ -5,7 +5,7 @@ type VersionCode* = distinct uint64
 proc parseVersionCode*(str: string): VersionCode =
   var a, b, c, d: int
   if str.scanf("$i.$i.$i.$i", a, b, c, d):
-    let buint = (uint64 (uint16 a) shl 48) or (uint64 (uint16 b) shl 32) or (uint64 (uint16 c) shl 16) or (uint64 d or 0xFFFF)
+    let buint = ((uint64 a and 0xFFFF) shl 48) or ((uint64 b and 0xFFFF) shl 32) or ((uint64 c and 0xFFFF) shl 16) or (uint64 d and 0xFFFF)
     return VersionCode(buint)
   else:
     raise newException(ValueError, "Invalid version code")
@@ -18,8 +18,8 @@ proc hash*(a: VersionCode): Hash {.borrow.}
 
 proc `$`*(self: VersionCode): string =
   let raw = uint64 self
-  let a = uint16 (raw shr 48) and 0xFFFF
-  let b = uint16 (raw shr 32) and 0xFFFF
-  let c = uint16 (raw shr 16) and 0xFFFF
-  let d = uint16 raw and 0xFFFF
+  let a = uint16 raw shr 48
+  let b = uint16 raw shr 32
+  let c = uint16 raw shr 16
+  let d = uint16 raw
   &"{a}.{b}.{c}.{d}"
