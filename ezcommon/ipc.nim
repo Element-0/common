4eeb5ffd98974d7bc4dbdbff9deab77922375a61
CodeHz <codehz@outlook.com>
2021-01-28 22:55:52 +0800

    add ipc

---
+++ ezcommon/ipc.nim
@@ -0,0 +1,32 @@
+import binpak, ./log
+
+type
+  RequestKind* {.pure.} = enum
+    req_ping
+    req_log
+    req_load
+
+  RequestPacket* = object
+    case kind*: RequestKind
+    of req_ping:
+      discard
+    of req_log:
+      logData*: LogData
+    of req_load:
+      modName*: string
+
+  ResponseKind* {.pure.} = enum
+    res_pong
+    res_failed
+    res_load
+
+  ResponsePacket* = object
+    case kind*: ResponseKind
+    of res_pong:
+      discard
+    of res_failed:
+      errMsg*: string
+    of res_load:
+      modPath*: string
+
+genBinPak(RequestPacket, ResponsePacket)
